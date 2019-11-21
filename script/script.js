55498ca7bd7b77dd9a3d881c60bb997527c685e9
oleoot <oleoot28@gmail.com>
2019-11-21 16:26:01 +0200

    Stable

--- script/script.js
+++ script/script.js
@@ -1,12 +1,112 @@
-// <script>
-//     $(document).ready(function() {
-//         $('.slider').slick({
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 3000,
-//             arrows: false
-//         });
-//     })
+// let plus = document.getElementById('plus__img');
+// let form = document.getElementById('order__form');
+// let orderWrap = document.getElementById('order__wrap');
+// form.addEventListener("click", function(e){
+//     if(e.target && e.target.id== 'plus__img'){
+//       let newNode = form.cloneNode(true);
+//       orderWrap.appendChild(newNode);
+//      }
+//  });
+
+
+
+
+
+// document.addEventListener('click',function(e){
+//     if(e.target && e.target.id== 'brnPrepend'){
+//           //do something
+//      }
+//  });
+// plus.addEventListener('click',function(event){
+//     if(event.srcElement.id == 'plus__img'){
+//       let newNode = form.cloneNode(true);
+//       orderWrap.appendChild(newNode);
+//      }
+//  });
+
+
+ // document.body.addEventListener( 'click', function ( event ) {
+ //   if( event.srcElement.id == 'btnSubmit' ) {
+ //     someFunc();
+ //   };
+ // } );
+// let plus = document.getElementsByClassName('plus__img');
+// let form = document.getElementById('order__form');
+// let orderWrap = document.getElementById('order__wrap');
+// [].forEach.call(plus,function(add){
+// add.addEventListener("click", more);
+// function more(){
+// let newNode = form.cloneNode(true);
+// orderWrap.appendChild(newNode);
+// }
+// });
+
+
+// let plus = document.getElementById('plus__img');
+// let form = document.getElementById('order__form');
+// let orderWrap = document.getElementById('order__wrap');
+// plus.onclick = function(){
+//   let newNode = form.cloneNode(true);
+//   orderWrap.appendChild(newNode);
+// };
+
+
+
+
+
+
+
+
+// [].forEach.call(like,function(el){
+//     el.addEventListener('click', function (e) {...})
+// });
+
+
+
+
+
+
+// РАБОЧИЙ КУСОК
+let plus = document.getElementById('plus__img');
+let first = document.getElementById('order__selectf');
+let second = document.getElementById('order__selects');
+let third = document.getElementById('order__selectt');
+let count = 1;
+
+let form = document.getElementById('order__form');
+let orderWrap = document.getElementById('order__wrap');
+plus.addEventListener("click", more);
+function more(){
+// let p = document.createElement("p");
+let newNode = first.cloneNode(true);
+let newNodes = second.cloneNode(true);
+let newNodet = third.cloneNode(true);
+// let p1 = p.appendChild(newNode);
+// let p2 = p.appendChild(newNodes);
+// let p3 = p.appendChild(newNodet);
+form.appendChild(newNode);
+form.appendChild(newNodes);
+form.appendChild(newNodet);
+}
+
+
+
+
+
+// let plus = document.getElementById('plus__img');
+// let first = document.getElementById('order__selectf');
+// let second = document.getElementById('order__selects');
+// let third = document.getElementById('order__selectt');
+// let count = 1;
 //
-// </script>
+// let form = document.getElementById('order__form');
+// let orderWrap = document.getElementById('order__wrap');
+// plus.addEventListener("click", more);
+// function more(){
+// let newNode = first.cloneNode(true);
+// let newNodes = second.cloneNode(true);
+// let newNodet = third.cloneNode(true);
+// form.appendChild(newNode);
+// form.appendChild(newNodes);
+// form.appendChild(newNodet);
+// }
